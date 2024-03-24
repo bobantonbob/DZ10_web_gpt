f939a3aa21d192608acfcee91c14ab63a741a6bd
bobantonbob <130400459+bobantonbob@users.noreply.github.com>
2024-03-24 20:21:33 +0200

    v1

--- root/settings.py
+++ root/settings.py
@@ -32,8 +32,8 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-# ALLOWED_HOSTS = ['quotes-antonbabenko.koyeb.app']
 ALLOWED_HOSTS = ['quotes-antonbabenko.koyeb.app']
+# ALLOWED_HOSTS = []
 
 # Application definition
 
